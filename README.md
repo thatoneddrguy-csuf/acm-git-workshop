c5cd650043602c8657f80dbdc354689cc5fbc821
James Ku <thatoneddrguy@csu.fullerton.edu>
2018-09-14 18:07:05 -0700

    add name to project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # acm-git-workshop
 Code that's being used during the ACM git workshop!
+I'm James
--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,6 @@
 int main()
 {
   std::cout << "Hello World!\n";
+  std::cout << "Hi I'm James\n";
   return 0;
 }
